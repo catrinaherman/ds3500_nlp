059a52befade99e7e21954be348481ccea420f8a
catrinaherman <herman.ca@northeastern.edu>
2025-04-07 15:34:39 -0400

    updated application file

--- npl_project/text_analysis_app.py
+++ npl_project/text_analysis_app.py
@@ -1,8 +1,5 @@
 from text_analysis_class import TextAnalysis
 import pprint as pp
-import plotly.graph_objects as go
-from sankey_subplots import text_to_word_sankey
-from sankey_subplots import sentiment_distribution_subplots
 
 
 def main():
@@ -20,75 +17,15 @@ def main():
     tt.load_text("USA_Today.txt", label="USA_Today")
     tt.load_text("The_Hill.txt", label="The_Hill")
 
-    pp.pprint(tt.data)
 
-    text_to_word_sankey(tt, k=5)
-    sentiment_distribution_subplots(tt)
+    tt.wordcount_sankey(k=5)
+    tt.sentiment_distribution_subplots()
 
 
 if __name__ == "__main__":
     main()
 
-# Sankey Visualization
 
-def wordcount_sankey(self, word_list=None, k=5):
-    labels = []           # All nodes in the diagram: text names + words
-    sources = []          # Index of source node (text label)
-    targets = []          # Index of target node (word)
-    values = []           # Thickness of the connection = word count
-    label_indices = {}    # Mapping from label to node index
-
-    # Getting all the text labels
-    text_labels = list(self.data["word_freqs"].keys())
-
-    # Picks which words to use:
-    if word_list:
-        selected_words = set(word_list)
-    else:
-        selected_words = set()
-        for label in text_labels:
-            most_common = self.data["word_freqs"][label].most_common(k)
-            selected_words.update([w for w, _ in most_common])
-
-    # Add text labels first
-    for label in text_labels:
-        label_indices[label] = len(labels)
-        labels.append(label)
-
-    # Then add word labels
-    for word in sorted(selected_words):
-        label_indices[word] = len(labels)
-        labels.append(word)
-
-    # Build text to word connections
-    for label in text_labels:
-        word_freq = self.data["word_freqs"][label]
-        for word in selected_words:
-            if word in word_freq:
-                sources.append(label_indices[label])
-                targets.append(label_indices[word])
-                values.append(word_freq[word])
-
-    # Create sankey diagram
-    fig = go.Figure(data=[go.Sankey(
-        node=dict(
-            pad=15,
-            thickness=20,
-            line=dict(color="black", width=0.5),
-            label=labels
-        ),
-        link=dict(
-            source=sources,
-            target=targets,
-            value=values
-        )
-    )])
-
-    fig.update_layout(title_text="Text-to-Word Sankey Diagram", font_size=10)
-    fig.show()
-
-    # Example
-    tt.wordcount_sankey(k=5)
 
 
 
