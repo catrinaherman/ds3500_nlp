248fa0a1d14d8ab1218331a56d07a400466636dd
mikamclean <mclean.mik@northeastern.edu>
2025-04-07 21:07:08 -0400

    Added comments

--- npl_project/text_analysis_app.py
+++ npl_project/text_analysis_app.py
@@ -1,9 +1,13 @@
 from text_analysis_class import TextAnalysis
-import pprint as pp
 
 def main():
+    # Initialize the text analysis object
     tt = TextAnalysis()
+
+    # Loading common English stopwords to filter out from the text
     tt.load_stop_words()
+
+    # Loading and labeling each news article file
     tt.load_text("ABC_1.txt", label="ABC1")
     tt.load_text("ABC_2.txt", label="ABC2")
     tt.load_text("BBC_1.txt", label="BBC1")
@@ -16,13 +20,25 @@ def main():
     tt.load_text("USA_Today.txt", label="USA_Today")
     tt.load_text("The_Hill.txt", label="The_Hill")
 
-    # tt.wordcount_sankey(k=5)
-    # tt.sentiment_distribution_subplots()
-    # tt.article_sentiment_comparison(sentiment_type='positive')
-    # tt.article_sentiment_comparison(sentiment_type='negative')
-    # tt.sentiment_scatter()
+    # Generate a sankey diagram to show which words are most commonly used
+    tt.wordcount_sankey(k=3)
+
+    # Create subplots showing sentiment distribution (positve and negative) per article
+    tt.sentiment_distribution_subplots()
+
+    # Create barcharts comparing the % of positive sentiment across all articles
+    tt.article_sentiment_comparison(sentiment_type='positive')
+
+    #Create barcharts comparing the % of negative sentiment across all articles
+    tt.article_sentiment_comparison(sentiment_type='negative')
+
+    # Plotting positive vs. negative sentiment % in a scatter plot
+    tt.sentiment_scatter()
+
+    # Create a radar chart to compare multiple features (ex: avg sentence length, positive/negative sentiment, etc)
     tt.radar_chart()
 
+# Runs the analysis when the script gets executed
 if __name__ == "__main__":
     main()
 
